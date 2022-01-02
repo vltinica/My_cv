8f06844133179bb3b2c76dcf6f3fd6bd08c001cd
vltinica <61639005+vltinica@users.noreply.github.com>
2022-01-02 15:22:44 -0500

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,78 @@
+const langArr = {
+  "profile-description-1": {
+    en: "After university, the first experience was gained in a small company WEB-ART in 2004-2006 on the territory of the Republic of Moldova; ",
+    fr: "Après l'université, la première expérience a été gagnée dans une petite compagnie l'ART DE WEB en 2004-2006 sur le territoire de la République de la Moldavie;",
+  },
+
+  "profile-description-2": {
+    en: "WEB-ART has developed one-page sites for individuals and legal entities (small businesses);",
+    fr: "L'ART DE WEB a développé des sites d'une page pour les individus et les entités juridiques (les petites entreprises)",
+  },
+
+  "profile-description-3": {
+    en: "My responsibilities included: accompanying the client at the initial stage, providing various assistance to the employees of our company in the design of layouts for sites and etc.;",
+    fr: "Mes responsabilités incluses : l'accompagnement du client au stade initial, fournir de diverses assistances aux employés de notre compagnie dans le design de dispositions pour les sites et etc. ;",
+  },
+
+  "profile-description-4": {
+    en: "For the last 15 years, I have been working in a train repair company;",
+    fr: "Depuis les derniers 15 ans, j'ai travaillé dans une compagnie de réparation de train ;",
+  },
+
+  "profile-description-5": {
+    en: "My strengths: ability to work in team, attention to detail, perseverance and a desire to learn new things;",
+    fr: "My strengths: ability to work in team, attention to detail, perseverance and a desire to learn new things;",
+  },
+
+  "profile-description-6": {
+    en: "Languages: English-French (intermediary), Russian, Romanian and Bulgarian.",
+    fr: "Langues : français - anglais (intermédiaire), russe, roumain et bulgare.",
+  },
+
+  // --------------------------------------------------------------------------------------------------------//
+
+  "company-block-description-1": {
+    en: "Tear-down, cleaning, assembling, and testing different mechanical, hydraulic and pneumatic valves and portions for rail brake systems;",
+    fr: "Le désassemblage, le nettoyage, l'assemblage et le test de différentes valves mécaniques, hydrauliques et pneumatiques pour les systèmes de freinage;",
+  },
+
+  "company-block-description-2": {
+    en: "Trained and supervised new employees;",
+    fr: "Nouveaux employés entraînés et supervisés;",
+  },
+
+  "company-block-description-3": {
+    en: "Followed and complied high-quality standards, 5S and safety requirements;",
+    fr: "Suivi et s'est conformé des normes de haute qualité, 5S et des exigences de sécurité;",
+  },
+
+  "company-block-description-4": {
+    en: "Fabricated small components and electrical wiring as needed;",
+    fr: "Les petites composantes fabriquées et l'installation électrique comme nécessaire;",
+  },
+
+  "company-block-description-5": {
+    en: "Completed production and quality assurance forms;",
+    fr: "Formes d'assurance de la qualité et de production accomplies;",
+  },
+
+  "company-block-description-6": {
+    en: "Participated actively in team meetings and group discussions; ",
+    fr: "Participe activement aux réunions d'équipe et aux discussions de groupe;",
+  },
+
+  "company-block-description-7": {
+    en: "Interfaced with engineering to accurately communicate information;",
+    fr: "Interface avec l'ingénierie pour exactement communiquer les informations;",
+  },
+
+  "company-block-description-8": {
+    en: "Actively participated in continuous improvement activities or events as required;",
+    fr: "Activement participe dans les activités d'améliorations continues ou les événements comme exigé;",
+  },
+
+  "company-block-description-9": {
+    en: "Ability to read and comprehend procedures written in English and French.",
+    fr: "La capacité de lire et comprendre les procédures écrites en anglais et en français.",
+  },
+};
